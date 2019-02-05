4a7ae832940c815135973577ea5f969ce17cad81
MorganSkye <46610639+MorganSkye@users.noreply.github.com>
2019-02-05 16:06:45 -0600

    Add files via upload

---
+++ Taber/WhatsWrong.java
@@ -0,0 +1,33 @@
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Scanner;
+
+
+
+
+
+public class WhatsWrong {
+
+	public static void main(String[] args) {
+		
+	LocalDateTime now = LocalDateTime.now();
+		
+		System.out.println("It is " + now);
+
+		DateTimeFormatter dtfShort = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
+		
+		System.out.println("Formatted = " + dtfShort.format(now));
+		
+		System.out.println("What is your birthday? (MM/dd/yyyy)");
+		
+		String b = scanner.nextLine();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+		LocalDate birthday = LocalDate.parse(b, formatter);
+		
+		System.out.println("Your birthday is = " + birthday);
+	}		
+}
+	
