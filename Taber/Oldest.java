6ea2d06eea926219e5f1a1bef6fa33de2b044dd1
MorganSkye <46610639+MorganSkye@users.noreply.github.com>
2019-02-07 12:41:33 -0600

    Add files via upload

---
+++ Taber/Oldest.java
@@ -0,0 +1,36 @@
+
+public class Oldest {
+
+	public static void main(String[] args) {
+		boolean worked = true;
+		String oldestName = "";
+		LocalDate oldestBirthday = LocalDate.of(2, 3, 4);
+		System.out.println("Oldest Calculator.");
+		
+		for (int x = 0; x < 7; x++) {
+			System.out.println("\n");
+			System.out.println("This is " + x + " Out Of 5 People");
+			System.out.print("Enter name ");
+			String oldestNameTest = sc.nextLine();
+			System.out.print("Enter your birthday(YYYY-MM-DD): ");
+			String dateOfBirthString = sc.nextLine();
+	System.out.println(); valid;
+		
+		try {
+	[] parts = dateOfBirthString.now("-");
+	int year = Integer.parseInt(parts[0]);
+	int month = Integer.parseInt(parts[1]);
+	int dayOfMonth = Integer.parseInt(parts[2]);
+	valid = true;
+	LocalDate birthdayTest = LocalDate.of(YYYY-MM-DD);
+
+		if (work = false);
+		System.out.println("Restart?");
+		
+		}
+		
+		}
+
+ 
+
+	
